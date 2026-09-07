bf749b8d700118b435d9f02dabeeae15e5b28815
Manabendu-ai <rikugithub@gmail.com>
2026-09-07 15:38:28 +0530

    toTaskList

---
+++ src/main/java/riku/spytask/backend/mappers/impl/TaskListManagerImpl.java
@@ -0,0 +1,42 @@
+package riku.spytask.backend.mappers.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import riku.spytask.backend.dto.TaskListDTO;
+import riku.spytask.backend.entity.TaskList;
+import riku.spytask.backend.mappers.TaskListManager;
+
+import java.util.Optional;
+
+@Component
+public class TaskListManagerImpl implements TaskListManager {
+
+    private final TaskMapperImpl taskMapper;
+
+    @Autowired
+    public TaskListManagerImpl(TaskMapperImpl taskMapper) {
+        this.taskMapper = taskMapper;
+    }
+
+    @Override
+    public TaskList toTaskList(TaskListDTO taskListDTO) {
+        return new TaskList(
+                taskListDTO.id(),
+                taskListDTO.title(),
+                taskListDTO.description(),
+                Optional.ofNullable(
+                        taskListDTO.tasks()
+                ).map(
+                        tasks -> tasks.stream()
+                                .map(taskMapper::toTask).toList())
+                        .orElse(null)
+                ,null,
+                null
+        );
+    }
+
+    @Override
+    public TaskListDTO toTaskListDTO(TaskList taskList) {
+        return null;
+    }
+}

035e3285a787805879342deb161518fadfbc52d0
Manabendu-ai <rikugithub@gmail.com>
2026-09-07 15:51:48 +0530

    implemented TaskListMapperImpl

--- src/main/java/riku/spytask/backend/mappers/impl/TaskListMapperImpl.java
+++ src/main/java/riku/spytask/backend/mappers/impl/TaskListMapperImpl.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import riku.spytask.backend.dto.TaskListDTO;
 import riku.spytask.backend.entity.TaskList;
+import riku.spytask.backend.entity.TaskStatus;
+import riku.spytask.backend.entity.Tasks;
 import riku.spytask.backend.mappers.TaskListMapper;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -38,7 +41,31 @@ public class TaskListMapperImpl implements TaskListMapper {
     @Override
     public TaskListDTO toTaskListDTO(TaskList taskList) {
         return new TaskListDTO(
+                taskList.getId(),
+                taskList.getTitle(),
+                taskList.getDescription(),
+                Optional.ofNullable(
+                        taskList.getTasks()
+                ).map(List::size)
+                        .orElse(0),
+                calTaskListProgress(taskList.getTasks()),
+                Optional.ofNullable(
+                        taskList.getTasks()
+                ).map(tasks -> tasks.stream().map(taskMapper::toDTO).toList())
+                        .orElse(null)
 
-        )
+        );
+    }
+
+    private Double calTaskListProgress(List<Tasks> tasks){
+        if(tasks == null){
+            return null;
+        }
+
+        long closed_count = tasks.stream().filter(
+                task -> TaskStatus.CLOSED == task.getStatus()
+        ).count();
+
+        return (double) closed_count/tasks.size();
     }
 }

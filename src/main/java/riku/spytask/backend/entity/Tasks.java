dc2863cd730b62621546a2200de9d80022d0f323
Manabendu-ai <rikugithub@gmail.com>
2026-09-07 14:32:32 +0530

    instantiated Task Entity

--- src/main/java/riku/spytask/backend/entity/Tasks.java
+++ src/main/java/riku/spytask/backend/entity/Tasks.java
@@ -1,8 +1,119 @@
 package riku.spytask.backend.entity;
 
-import jakarta.persistence.Entity;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
-@Entity 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tasks")
 public class Tasks {
     
+    @Id 
+    @GeneratedValue(strategy=GenerationType.UUID)
+    @Column(name = "id", updatable=false, nullable=false)
+    private UUID id;
+
+    @Column(name = "title", nullable=false)
+    private String title;
+
+    @Column(name = "description", nullable=false)
+    private String description;
+
+    @Column(name="due_date")
+    private LocalDateTime dueDate;
+
+    @Column(name = "status", nullable=false)
+    private TaskStatus status;
+
+    @Column(name = "status", nullable=false)
+    private TaskPriority priority;
+
+    @Column(name = "created_at", nullable=false)
+    private LocalDateTime createdAt;
+    
+    @Column(name = "updated_at", nullable=false)
+    private LocalDateTime updatedAt;
+
+    public Tasks(UUID id, String title, String description, LocalDateTime dueDate, TaskStatus status,
+            TaskPriority priority, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.status = status;
+        this.priority = priority;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
 }

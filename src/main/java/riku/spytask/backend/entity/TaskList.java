052cbb7158ef25aba91bb9f611e914f6d29c52c1
Manabendu-ai <rikugithub@gmail.com>
2026-09-07 14:44:21 +0530

    relationship between task and taskList

--- src/main/java/riku/spytask/backend/entity/TaskList.java
+++ src/main/java/riku/spytask/backend/entity/TaskList.java
@@ -1,13 +1,17 @@
 package riku.spytask.backend.entity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
+
+import org.springframework.scheduling.config.Task;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity 
@@ -23,6 +27,9 @@ public class TaskList {
 
     @Column(name = "description", nullable=false)
     private String description;
+
+    @OneToMany(mappedBy="taskList")
+    private List<Task> tasks;
 
     @Column(name = "created_at", nullable=false)
     private LocalDateTime createdAt;

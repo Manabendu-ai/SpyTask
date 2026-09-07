a744ef582b39341b9ce779962a7986ebe7cca6a5
Manabendu-ai <rikugithub@gmail.com>
2026-09-07 16:18:32 +0530

    Implemented TaskListController

---
+++ src/main/java/riku/spytask/backend/controllers/TaskListController.java
@@ -0,0 +1,34 @@
+package riku.spytask.backend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import riku.spytask.backend.dto.TaskListDTO;
+import riku.spytask.backend.mappers.TaskListMapper;
+import riku.spytask.backend.services.TaskListService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/tasks-lists")
+public class TaskListController {
+
+    private final TaskListService taskListService;
+    private final TaskListMapper taskListMapper;
+
+    @Autowired
+    public TaskListController(TaskListService taskListService, TaskListMapper taskListMapper) {
+        this.taskListService = taskListService;
+        this.taskListMapper = taskListMapper;
+    }
+
+
+    @GetMapping
+    public List<TaskListDTO> listTaskLists(){
+        return taskListService.listTaskLists()
+                .stream()
+                .map(taskListMapper::toTaskListDTO)
+                .toList();
+    }
+}

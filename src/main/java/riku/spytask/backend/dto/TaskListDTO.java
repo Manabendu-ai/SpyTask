4d01e2930c2a2c8bb1cf2b781b2f2f9c6be35672
Manabendu-ai <rikugithub@gmail.com>
2026-09-07 15:17:05 +0530

    TaskListDTO

---
+++ src/main/java/riku/spytask/backend/dto/TaskListDTO.java
@@ -0,0 +1,15 @@
+package riku.spytask.backend.dto;
+
+import java.util.List;
+import java.util.UUID;
+
+public record TaskListDTO(
+        UUID id,
+        String title,
+        String description,
+        Integer count,
+        Double progress,
+        List<TaskDTO> tasks
+
+) {
+}

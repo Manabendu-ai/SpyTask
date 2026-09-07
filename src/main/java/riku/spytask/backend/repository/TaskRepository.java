a80f52ab50bf145601161d0bf84f479f6de61b82
Manabendu-ai <rikugithub@gmail.com>
2026-09-07 16:04:58 +0530

    created findByTaskListIdAndId and findByTaskListId

--- src/main/java/riku/spytask/backend/repository/TaskRepository.java
+++ src/main/java/riku/spytask/backend/repository/TaskRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import riku.spytask.backend.entity.Tasks;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Tasks, UUID> {
+    List<Tasks> findByTaskListId(UUID id);
+    Optional<Tasks> findByTaskListIdAndId(UUID taskListId, UUID id);
 }

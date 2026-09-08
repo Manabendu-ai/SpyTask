63f86fa6cac6aab22b87f47a0676a19ed18c18ba
Manabendu-ai <rikugithub@gmail.com>
2026-09-08 13:34:50 +0530

    implemented deleteByTaskListAndId in the repo

--- src/main/java/riku/spytask/backend/repository/TaskRepository.java
+++ src/main/java/riku/spytask/backend/repository/TaskRepository.java
@@ -12,4 +12,5 @@ import java.util.UUID;
 public interface TaskRepository extends JpaRepository<Tasks, UUID> {
     Optional<List<Tasks>> findByTaskListId(UUID id);
     Optional<Tasks> findByTaskListIdAndId(UUID taskListId, UUID id);
+    void deleteByTaskListAndId(UUID taskListId, UUID id);
 }

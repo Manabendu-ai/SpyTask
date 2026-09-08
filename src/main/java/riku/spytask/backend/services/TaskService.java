296d004fb2e1a146820f0401a6342c49f9d99e16
Manabendu-ai <rikugithub@gmail.com>
2026-09-08 12:44:12 +0530

    getAllTasks

--- src/main/java/riku/spytask/backend/services/TaskService.java
+++ src/main/java/riku/spytask/backend/services/TaskService.java
@@ -2,8 +2,10 @@ package riku.spytask.backend.services;
 
 import riku.spytask.backend.entity.Tasks;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskService {
     Tasks createTask(UUID taskListId, Tasks tasks);
+    List<Tasks> getAllTasks();
 }

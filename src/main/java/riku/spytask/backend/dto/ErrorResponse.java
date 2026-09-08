fcdb228e0f7577163210b6a2fd5b59344f02c728
Manabendu-ai <rikugithub@gmail.com>
2026-09-08 10:33:24 +0530

    implemented GlobalExceptionHandler

--- src/main/java/riku/spytask/backend/dto/ErrorResponse.java
+++ src/main/java/riku/spytask/backend/dto/ErrorResponse.java
@@ -1,5 +1,9 @@
 package riku.spytask.backend.dto;
 
+import jakarta.websocket.OnError;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
 public record ErrorResponse(
         int status,
         String message,

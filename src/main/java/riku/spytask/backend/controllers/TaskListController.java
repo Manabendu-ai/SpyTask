384150afeb58580c66c9c86fd5a75eaeca850426
Manabendu-ai <rikugithub@gmail.com>
2026-09-08 15:00:58 +0530

    Added one Message Mapping to taskList controller

--- src/main/java/riku/spytask/backend/controllers/TaskListController.java
+++ src/main/java/riku/spytask/backend/controllers/TaskListController.java
@@ -1,6 +1,7 @@
 package riku.spytask.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.*;
 import riku.spytask.backend.dto.TaskListDTO;
 import riku.spytask.backend.mappers.TaskListMapper;
@@ -22,7 +23,7 @@ public class TaskListController {
         this.taskListMapper = taskListMapper;
     }
 
-
+    @MessageMapping("/listTaskList")
     @GetMapping("/")
     public List<TaskListDTO> listTaskLists(){
         return taskListService.listTaskLists()

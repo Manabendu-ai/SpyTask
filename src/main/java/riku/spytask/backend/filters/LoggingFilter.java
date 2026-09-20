6b89903902507754d2e457093c890515c22f037e
Manabendu-ai <rikugithub@gmail.com>
2026-09-20 15:12:02 +0530

    extending Jakarta Servlet Filter

--- src/main/java/riku/spytask/backend/filters/LoggingFilter.java
+++ src/main/java/riku/spytask/backend/filters/LoggingFilter.java
@@ -1,7 +1,9 @@
 package riku.spytask.backend.filters;
 
+import jakarta.servlet.Filter;
 import org.springframework.stereotype.Component;
 
 @Component
-public class LoggingFilter {
+public class LoggingFilter extends Filter {
+
 }

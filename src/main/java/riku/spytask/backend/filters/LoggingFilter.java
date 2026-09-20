e0c8ce7e3299779f103f343b470ed401b8f3ac92
Manabendu-ai <rikugithub@gmail.com>
2026-09-20 15:12:26 +0530

    implementing do filter method

--- src/main/java/riku/spytask/backend/filters/LoggingFilter.java
+++ src/main/java/riku/spytask/backend/filters/LoggingFilter.java
@@ -1,9 +1,15 @@
 package riku.spytask.backend.filters;
 
-import jakarta.servlet.Filter;
+import jakarta.servlet.*;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class LoggingFilter extends Filter {
 
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
 }
